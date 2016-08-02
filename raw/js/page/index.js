99b0ec99ef483e4bd9c8024eb7a07c5c8e33a364
linz1989 <jialin2013@qq.com>
2016-08-02 14:20:24 +0800

    3

--- raw/js/page/index.js
+++ raw/js/page/index.js
@@ -1,73 +1,23 @@
-//*********************************************************************
-// 检测是否支持某个CSS3样式的方法
-//*********************************************************************
-function isSupportCss3(style) {
-    var prefix = ['webkit', 'Moz', 'ms', 'o'],
-        i,
-        humpString = [],
-        htmlStyle = document.documentElement.style,
-        _toHumb = function (string) {
-            return string.replace(/-(\w)/g, function ($0, $1) {
-                return $1.toUpperCase();
-            });
-        };
-
-    for (i in prefix) {
-        humpString.push(_toHumb(prefix[i] + '-' + style));
-    }
-    humpString.push(_toHumb(style));
-
-    for (i in humpString)
-        if (humpString[i] in htmlStyle) return true;
-    return false;
-}
-//********************************************************************
-//对localStorage的封装
-//*******************************************************************
-function ls(key,value){
-    if(localStorage){
-        if(value){
-            if(localStorage.setItem) return localStorage.setItem(key,value);
+$(function(){
+    var homeData,
+        i = 0;
+    
+    $.get("../json/home.json",{ "_t" : (+new Date())},function(res){
+        homeData = res;
+        var silderImgs = homeData.homeSlider,
+            htmlStr = "";
+        for(i=0;i<silderImgs.length;i++){
+            htmlStr +="<li><img src='"+silderImgs[i]+"' /></li>";
         }
-        else if(localStorage.getItem){
-            return localStorage.getItem(key);
-        }
-    }
-    return false;
-}
-
-///获取地址栏参数
-function getParam(){
-    var paramObj = {},
-        paramArr = [],
-        i,
-        arr,
-        paramUrl = location.href.split("?")[1];
-    if(paramUrl){
-        paramArr = paramUrl.split("&");
-        for(i=0;i<paramArr.length;i++){
-            arr = paramArr[i].split("=");
-            if(arr.length==2){
-                paramObj[arr[0]] = arr[1];
-            }
-        }
-    }
-    return paramObj;
-}
-
-$(function () {
-    var $win = $(window), supportTouch = "ontouchend" in document;
-
-    ///////////////////////////////////////////////////////////点击菜单弹出
-    var menuListArr = $("div.menu-wrap>div>ul>li");
-    menuListArr[supportTouch ? "click" : "hover"](function(event){ doHandlerMenu(this); event.stopPropagation(); });
-
-    function doHandlerMenu(menu){
-        if(menu.className != "active"){
-            menuListArr.removeClass("active");
-            menu.className = "active";
-        }
-        else menu.className = "";
-    }
-
-});
+        $("#slider-wrap>ul.slides").html(htmlStr);
+        
+        $("#slider-wrap").flexslider({
+            animation: "slide",
+            slideshowSpeed : 3000,
+            slideshow : true,
+            animationLoop : true,
+            pauseOnAction : false,
+            pauseOnHover : false
+        });
+    },"json");
+})
